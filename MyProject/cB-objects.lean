95d1e38c4c253f4bf9f40cafb6fbbaaf7d5dc4e3
Ran Tao <rant2@andrew.cmu.edu>
2026-07-14 16:59:05 -0400

    Added definitions for countable, Borel objects (ER, graphs)

---
+++ MyProject/cB-objects.lean
@@ -0,0 +1,39 @@
+/- This file is for defining cBer and countable Borel graphs. -/
+
+import Mathlib.Topology.Basic
+import Mathlib.MeasureTheory.Constructions.Polish.Basic
+import Mathlib.Tactic
+import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Data.Set.Card
+
+variable {X : Type*} [MeasurableSpace X]
+
+
+/- Defining cBer -/
+
+structure measR (E : X → X → Prop) where
+  Meas : MeasurableSet { p : X × X | E p.1 p.2 }
+
+structure ctblER (E : X → X → Prop) extends Equivalence E where
+  Ctbl : ∀ (x : X), { y : X | E y x }.Countable
+
+structure cBer [StandardBorelSpace X] (E: X → X → Prop) extends measR E, ctblER E
+
+
+
+/- Defining locally countable Borel graphs-/
+
+structure measGraph (G : SimpleGraph X) where
+  Meas : MeasurableSet { p : X × X | G.Adj p.1 p.2 }
+
+structure lcGraph (G : SimpleGraph X) where
+  Ctbl : ∀ (x : X), ({ y : X | G.Adj x y }).Countable
+
+structure lfGraph (G : SimpleGraph X) where
+  Finite : ∀ (x : X), ({ y : X | G.Adj x y }).Finite
+
+structure lbGraph (n : ℕ) (G : SimpleGraph X) where
+  Bounded : ∀ (x : X), ({ y : X | G.Adj x y }).ncard ≤ n
+
+structure lcBGraph (G : SimpleGraph X) extends measGraph G, lcGraph G where
+  stdBorel : StandardBorelSpace X
