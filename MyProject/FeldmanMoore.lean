2d1da6f565fc95929223dea5664ba40524018578
Ran Tao <rant2@andrew.cmu.edu>
2026-07-15 16:23:50 -0400

    no

--- MyProject/FeldmanMoore.lean
+++ MyProject/FeldmanMoore.lean
@@ -25,6 +25,4 @@ theorem FeldmanMoore {X : Type*} [MeasurableSpace X] [StandardBorelSpace X] {E :
     ∃ (G : Type) (_ : Group G) (_ : TopologicalSpace G) (_ : MeasurableSpace G) (_ : MulAction G X),
     BorelSpace G ∧ @cdGroup G _ _ ∧ MeasurableSMul₂ G X
     ∧ MulAction.orbitRel G X = E
-    := by
-    --dummyyyyy England 1-0
-    sorry
+    := by sorry
