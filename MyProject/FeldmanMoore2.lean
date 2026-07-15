9d075644f3144f33d06049526f1defbc1d3f1250
Zelong Li <zelongl@andrew.cmu.edu>
2026-07-15 16:33:11 -0400

    new file

---
+++ MyProject/FeldmanMoore2.lean
@@ -0,0 +1,28 @@
+import MyProject.LusinNovikovFull
+import MyProject.CBobjects
+import Mathlib.MeasureTheory.MeasurableSpace.Instances
+
+namespace FeldmanMoore
+
+set_option autoImplicit false
+
+
+/-- Feldman-Moore in the context of edge-colorings: Suppose G is a Borel graph
+    on std Borel X, then G admits a countable Borel edge-coloring
+-/
+
+
+
+theorem FeldmanMooreEdgeColoring {X : Type*} [MeasurableSpace X] [StandardBorelSpace X]
+  {G : SimpleGraph X} (lcbG : lcBGraph G) :
+    ∃ (c : ordEdgeSet G → ℕ) , isMeasEdgeCol G c  := sorry
+
+/- Feldman-Moore for CBERs: Suppose that E is a CBER on std Borel X, then there is
+    a countable group Γ and a Borel action Γ → X whose orbit equivalence relation is E,
+    i.e. x E y iff ∃ γ ∈ Γ (γ * x = y).
+-/
+theorem FeldmanMoore {X : Type*} [MeasurableSpace X] [StandardBorelSpace X] {E : X → X → Prop} (cberE : cBer E) :
+    ∃ (G : Type) (_ : Group G) (_ : TopologicalSpace G) (_ : MeasurableSpace G) (_ : MulAction G X),
+    BorelSpace G ∧ @cdGroup G _ _ ∧ MeasurableSMul₂ G X
+    ∧ MulAction.orbitRel G X = E
+    := by sorry
