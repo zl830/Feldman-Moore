53528013a3402b98710c5f61fccc7279bfeabdef
Zelong Li <zelongl@andrew.cmu.edu>
2026-07-17 11:34:22 -0400

    fm 2 almost done

--- MyProject/FeldmanMoore.lean
+++ MyProject/FeldmanMoore.lean
@@ -103,26 +103,62 @@ theorem FeldmanMooreEdgeColoring_uncountable {X : Type*} [MeasurableSpace X] [St
     i.e. x E y iff ∃ γ ∈ Γ (γ * x = y).
 -/
 open Classical in
-theorem FeldmanMoore {X : Type*} [MeasurableSpace X] [StandardBorelSpace X] {E : X → X → Prop} (cberE : cBer E) :
+theorem FeldmanMoore {X : Type*} (hNotCtbl : ¬Countable X) [MeasX : MeasurableSpace X]
+    [stdborelX : StandardBorelSpace X] {E : X → X → Prop} (cberE : cBer E) :
     ∃ (G : Type) (_ : Group G) (_ : TopologicalSpace G) (_ : MeasurableSpace G) (_ : MulAction G X),
     BorelSpace G ∧ @cdGroup G _ _ ∧ MeasurableSMul₂ G X
-    ∧ MulAction.orbitRel G X = E
-    := by
+    ∧ MulAction.orbitRel G X = E := by
   let graph := SimpleGraph.fromEdgeSet {x : Sym2 X | ∃ x1 x2 : x , x1 ≠ x2 ∧ E x1 x2}
   rcases cberE with ⟨h1, h2, h3⟩
   have lcbG : lcBGraph graph := by
     constructor
     · constructor
       unfold ordEdgeSet
-      unfold graph
-      simp only [SimpleGraph.fromEdgeSet]
-
+      have : {p : X × X| graph.Adj p.1 p.2} = {p : X × X | E p.1 p.2} \ Set.diagonal X := by
+        ext x
+        constructor
+        · intro hx
+          rcases hx with ⟨hx1, hx2⟩
+          simp only [Set.mem_sdiff, Set.mem_setOf_eq, Set.mem_diagonal_iff]
+          constructor
+          · simp only [Sym2.toRel_prop, Set.mem_setOf_eq] at hx1
+            rcases hx1 with ⟨x1, ⟨x2, x3⟩⟩
+            have in1 : x.1 = x1 ∨ x.1 = x2 := by grind
+            have in2 : x.2 = x1 ∨ x.2 = x2 := by grind
+            rcases in1 with eq11 | eq12
+            · rcases in2 with  eq21 | eq22
+              · grind
+              · grind
+            · rcases in2 with  eq21 | eq22
+              · apply h2.symm
+                grind
+              · grind
+          · apply hx2
+        · intro hx
+          rcases hx with ⟨hx1, hx2⟩
+          simp at hx1
+          simp only [Set.mem_diagonal_iff] at hx2
+          unfold graph
+          simp only [SimpleGraph.fromEdgeSet_adj, Set.mem_setOf_eq]
+          simp only [ne_eq, Subtype.exists, Sym2.mem_iff, exists_and_right, Subtype.mk.injEq,
+            exists_prop, exists_eq_or_imp, ↓existsAndEq, true_and]
+          constructor
+          · constructor
+            use x.2
+            grind
+          · exact hx2
+      rw [this]
+      apply MeasurableSet.diff
+      · apply h1.Meas
+      · have := stdborelX.instMeasurableEq
+        apply this.measurableSet_diagonal
     · sorry
-    · sorry
-  have h := FeldmanMooreEdgeColoring lcbG
+    · apply stdborelX
+  have h := FeldmanMooreEdgeColoring_uncountable hNotCtbl lcbG
   rcases h with ⟨c, measc⟩
   let classes : ℕ → Set X := fun n => {x : X | ∃ e : ordEdgeSet graph, c e = n ∧ x = e.1.1}
-  let invols : ℕ → X → X := fun n x => if x ∈ classes n ∧ (∃ y : X , graph.Adj x y ∧ (c ⟨(x,y) , ?_⟩ = n)) then sorry
+  let invols : ℕ → X → X := fun n x => if x ∈ classes n ∧
+    (∃ y : X , graph.Adj x y ∧ (c ⟨(x,y) , ?_⟩ = n)) then sorry
     else x
   swap
   · sorry
@@ -131,6 +167,23 @@ theorem FeldmanMoore {X : Type*} [MeasurableSpace X] [StandardBorelSpace X] {E :
     unfold invols
     sorry
   let G := Monoid.CoprodI fun n : ℕ => Multiplicative (ZMod 2)
+  let topG : TopologicalSpace G := ⊥
+  have disctopG: DiscreteTopology G := by
+    exact discreteTopology_iff_forall_isOpen.mpr fun s ↦ trivial
+  have discspaceG : discreteSpace G := by
+    constructor
+    apply disctopG
+  have topgroupG : IsTopologicalGroup G := by
+    infer_instance
+  have discgroupG : discreteGroup G := by
+    constructor
+    · apply discspaceG
+    · apply topgroupG
+  have ctblG : Countable G := by
+    unfold G
+    simp [Monoid.CoprodI]
+    sorry
+  let measG : MeasurableSpace G := ⊥
   let GactX : MulAction G X := MulAction.ofEndHom (Monoid.CoprodI.lift fun n : ℕ => {
     toFun mulZ := match mulZ.toAdd with
       | 0 => id
@@ -146,23 +199,22 @@ theorem FeldmanMoore {X : Type*} [MeasurableSpace X] [StandardBorelSpace X] {E :
       | 1, 0 => rfl
       | 1, 1 => funext x; symm; apply is_invol n x
   })
-  have (n : ℕ) (x : X) : (Monoid.CoprodI.of (i := n) (Multiplicative.ofAdd 1) : G) • x = invols n x := by rfl
-  let topG : TopologicalSpace G := ⊥
-  have : DiscreteTopology G := by
-    exact discreteTopology_iff_forall_isOpen.mpr fun s ↦ trivial
-  have discspaceG : discreteSpace G := by
-    constructor
-    apply this
-  have topgroupG : IsTopologicalGroup G := by
-    infer_instance
-  have discgroupG : discreteGroup G := by
-    constructor
-    · apply discspaceG
-    · apply topgroupG
-  have ctblG : Countable G := by
-    unfold G
-    simp [Monoid.CoprodI]
+  have (n : ℕ) (x : X) : (Monoid.CoprodI.of (i := n) (Multiplicative.ofAdd 1) : G) • x = invols n x
+    := by rfl
+  use G, Monoid.CoprodI.instGroup fun n ↦ Multiplicative (ZMod 2), topG, measG, GactX
+  constructor
+  · have discmeasG : DiscreteMeasurableSpace G := by
+      refine { forall_measurableSet := ?_ }
+      intro s
+      sorry
     sorry
-  have cdG : cdGroup G := by
+  constructor
+  · constructor
+    · apply ctblG
+    · apply discgroupG
+  constructor
+  · sorry
 
   sorry
+
+end FeldmanMoore
